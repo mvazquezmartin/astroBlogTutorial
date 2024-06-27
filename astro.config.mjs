e7c2ee946582983da35ddd997ea6300b475a40d1
mvazquez <matias.vazquezmartin@gmail.com>
2024-06-26 22:09:48 -0300

    feat: deploy github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [preact()]
+  site: 'https://mvazquezmartin.github.io/',
+  base: 'astroBlogTutorial',
+  integrations: [preact()],
 });
